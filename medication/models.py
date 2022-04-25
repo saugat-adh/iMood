8eb64e5f2200457316f2c0f3968c86614fa96000
Rampagg3 <saugatadhikari2010@gmail.com>
2022-04-25 14:54:04 +0545

    Medications Files

--- medication/models.py
+++ medication/models.py
@@ -1,16 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
 from user.models import MyUser
+import django.utils.timezone
 
 # Create your models here.
-
 
 class Medications(models.Model):
     name = models.CharField(max_length=20)
     created_by = models.ForeignKey(MyUser, on_delete=models.CASCADE, default= 0)
     dosage = models.CharField(max_length=50)
-    time = models.CharField(max_length=50)
+    #time = models.CharField(max_length=50)
     
     def __str__(self):
         return self.name
-    
