0ecc67848d40e742921307b97c06996ef08e7e2d
Rampagg3 <saugatadhikari2010@gmail.com>
2022-05-07 11:02:20 +0545

    Setting changes

--- iMood/settings.py
+++ iMood/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['https://imood-web.azurewebsites.net']
+ALLOWED_HOSTS = ['imood-web.azurewebsites.net']
 
 
 # Application definition
