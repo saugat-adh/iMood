066a9a956be31194e741897287da920b1a5ceb0b
Rampagg3 <saugatadhikari2010@gmail.com>
2022-05-07 11:42:51 +0545

    Azure workflow

--- iMood/settings.py
+++ iMood/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['imood-webapp.azurewebsites.net']
+ALLOWED_HOSTS = [config('HOST')]
 
 
 # Application definition
